b5b7c0e3e5dd96c09c065489de3702103a3a33a9
krotrn <karankumarsahtkg2018@gmail.com>
2025-05-22 22:31:48 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import { MatchesTable } from "@/components/matches";
 
 export default async function Home() {
   const matchesResponse = await getMatches();
-  console.log(matchesResponse.data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
       {matchesResponse.success ? <MatchesTable matchesResponse={matchesResponse} /> : (
         <p className="text-red-500">{matchesResponse.message}</p>
       )}
